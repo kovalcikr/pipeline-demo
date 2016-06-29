307bc9bcea31479912237f211a53a6204a15cb86
Radovan Kovalčik <radovankovalcik@gmail.com>
2016-06-29 21:33:17 +0200

    fail test

--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -7,6 +7,6 @@ public class JavaTest {
         @Test
         public void multiplicationOfZeroIntegersShouldReturnZero() {
                 // assert statements
-                assertEquals("10 x 0 must be 0", 0, 0 * 0);
+                assertEquals("10 x 0 must be 0", 1, 0 * 0);
         }
 }

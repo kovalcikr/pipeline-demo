90035cafe323fe847242564bda14d4c865b541f0
Radovan Kovalčik <radovankovalcik@gmail.com>
2016-06-29 21:26:07 +0200

    fix test

--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -6,8 +6,6 @@ public class JavaTest {
 
         @Test
         public void multiplicationOfZeroIntegersShouldReturnZero() {
-                MyClass tester = new MyClass(); // MyClass is tested
-
                 // assert statements
                 assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0));
                 assertEquals("0 x 10 must be 0", 0, tester.multiply(0, 10));
